9d71e4443902d9d763cf7222790bdf19bf4c6710
Sanjula Karunarathna <sanjulakarunarathna@Sanjulas-MacBook-Pro.local>
2022-07-10 17:42:46 +0530

    Product desplay

---
+++ sticker-pro-client/components/Layout.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Head from 'next/head' 
+import Link from 'next/link'
+
+function Layout({title, children }) {
+  return (
+    <>
+        <Head>
+            <title>{title ? title + '- Sticker pro' : 'Sticker pro'}</title>
+            <meta name="description" content="Ecommerce application" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className='flex flex-col justify-between min-h-screen' >
+            <header>
+            <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+                <Link href="/">
+                    <a className="text-lg font-bold">Sticker pro</a>
+                </Link>
+                <div>
+                <Link href="/cart">
+                    <a className="p-2">Cart</a>
+                </Link>
+                <Link href="/login">
+                    <a className="p-2">Login</a>
+                </Link>
+                </div>
+            </nav>
+            </header>
+            <main className="container m-auto mt-4 px-4">{children}</main>
+            <footer className="flex h-10 justify-center items-center shadow-inner">
+                <p>Copyright © 2022 Sticker pro</p>
+            </footer>
+        </div>
+    </>
+  )
+}
+
+export default Layout
