1439de84a9a05cfd393dd5195468cb8c883ea0d7
Sanjula Karunarathna <sanjulakarunarathna@Sanjulas-MacBook-Pro.local>
2022-07-11 02:16:27 +0530

    add shopping cart functionality

--- sticker-pro-client/components/Layout.js
+++ sticker-pro-client/components/Layout.js
@@ -1,38 +1,47 @@
-import React from 'react'
-import Head from 'next/head' 
-import Link from 'next/link'
+import Head from 'next/head';
+import Link from 'next/link';
+import React, { useContext } from 'react';
+import { Store } from '../utils/Store';
 
-function Layout({title, children }) {
+export default function Layout({ title, children }) {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <>
-        <Head>
-            <title>{title ? title + '- Sticker pro' : 'Sticker pro'}</title>
-            <meta name="description" content="Ecommerce application" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <div className='flex flex-col justify-between min-h-screen' >
-            <header>
-            <nav className="flex h-12 items-center px-4 justify-between shadow-md">
-                <Link href="/">
-                    <a className="text-lg font-bold">Sticker pro</a>
-                </Link>
-                <div>
-                <Link href="/cart">
-                    <a className="p-2">Cart</a>
-                </Link>
-                <Link href="/login">
-                    <a className="p-2">Login</a>
-                </Link>
-                </div>
-            </nav>
-            </header>
-            <main className="container m-auto mt-4 px-4">{children}</main>
-            <footer className="flex h-10 justify-center items-center shadow-inner">
-                <p>Copyright © 2022 Sticker pro</p>
-            </footer>
-        </div>
-    </>
-  )
-}
+      <Head>
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-export default Layout
+      <div className="flex min-h-screen flex-col justify-between ">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">amazona</a>
+            </Link>
+            <div>
+              <Link href="/cart">
+                <a className="p-2">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </span>
+                  )}
+                </a>
+              </Link>
+              <Link href="/login">
+                <a className="p-2">Login</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2022 Amazona</p>
+        </footer>
+      </div>
+    </>
+  );
+}
